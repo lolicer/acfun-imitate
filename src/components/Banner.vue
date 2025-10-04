7a709de36c5413e776b066c5230b84c15824b6ce
lolicer <13549177+lolicer@user.noreply.gitee.com>
2025-10-04 12:32:14 +0800

    初始化提交

---
+++ src/components/Banner.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="content">
+        <img src="../assets/images/Banner.png" alt="">
+    </div>
+</template>
+
+<style scoped src="./Banner.css"></style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    About
+</template>
+
+<style scoped></style>
