3de7eff418547e30d6ff90b29600c44b5057f951
lolicer <120192658+lolicer@users.noreply.github.com>
2026-02-02 21:18:12 +0800

    修复TruncatedText.vue边界情况下错误渲染省略号的BUG，优化文本首字符为全角字符时视觉偏右的情况

--- src/components/public/TruncatedText.vue
+++ src/components/public/TruncatedText.vue
@@ -5,7 +5,23 @@ const props = defineProps<{
     maxLine: number
 }>()
 
-const textRef = ref<HTMLElement>(null)
+const shouldTrimStart = [
+    '【',
+    '“',
+    '《',
+    '〈',
+    '「',
+    '『',
+    '‘',
+    '（',
+    '〔',
+    '〖',
+    '［',
+    '｛'
+].includes(props.text[0])
+const textIndent = shouldTrimStart ? '-0.5em' : '0'
+
+const textRef = ref<HTMLElement | null>(null)
 const tooltip = ref('')
 const checkOverflow = () => {
     if (textRef.value) {
@@ -24,21 +40,21 @@ onMounted(() => {
 </script>
 
 <template>
-    <div
-        ref="textRef"
-        :title="tooltip"
-        :style="{
-            display: '-webkit-box',
-            '-webkit-line-clamp': props.maxLine,
-            'line-clamp': props.maxLine,
-            '-webkit-box-orient': 'vertical',
-            overflow: 'hidden',
-            'text-overflow': 'ellipsis'
-        }"
-    >
+    <div class="truncated-text" ref="textRef" :title="tooltip">
         <slot name="article-icon"></slot>
         {{ props.text }}
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.truncated-text {
+    text-indent: v-bind(textIndent);
+
+    display: -webkit-box;
+    -webkit-line-clamp: v-bind('props.maxLine');
+    line-clamp: v-bind('props.maxLine');
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    /* text-overflow: ellipsis; -webkit-line-clamp自带，写上这个会导致边界情况下出BUG（如：maxLine为2时，在第一行就截断并显示省略号，但没有title） */
+}
+</style>
