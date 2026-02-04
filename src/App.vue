97e12755f94f5b0ebfbd8e860a597b7a1a23ef49
lolicer <120192658+lolicer@users.noreply.github.com>
2026-02-04 13:49:15 +0800

    添加底部

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,30 @@ import Test from './components/Test.vue'
     <Banner />
     <Navigation />
     <RouterView />
-    <hr />
-    <!-- <Test/> -->
+    <div id="bottom">
+        <img src="/src/assets/icons/acfunlogo.svg" alt="" />
+        <div id="bottom-text">Copyright © 2007-2022 AcFun. 保留所有权利</div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#bottom {
+    width: 100%;
+    height: 120px;
+    margin: 50px 0 0 0;
+    background-color: var(--color-gray-4);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+#bottom > img {
+    height: 40px;
+    padding-top: 26px;
+    color: var(--color-gray-1);
+}
+#bottom-text {
+    padding-top: 10px;
+    color: var(--color-gray-1);
+    text-align: center;
+}
+</style>
