35426f5c2750f26d9df8616df18fb38bc307e3c3
lolicer <120192658+lolicer@users.noreply.github.com>
2026-02-16 23:57:08 +0800

    添加监听，使Bangumi的Banner不可见时TitleBar为白色，可见时为透明

--- src/views/BangumiView/BangumiLayout.vue
+++ src/views/BangumiView/BangumiLayout.vue
@@ -2,16 +2,28 @@
 import Bottom from '@/components/public/Bottom.vue'
 import TitleBar from '@/components/TitleBar/TitleBar.vue'
 import BangumiContent from './BangumiContent/BangumiContent.vue'
+import { ref } from 'vue'
+
+const isBannerVisible = ref<boolean>(true)
 </script>
 
 <template>
-    <TitleBar class="bangumi-titlebar"/>
-    <BangumiContent/>
+    <TitleBar
+        class="bangumi-titlebar"
+        :class="{ transparent: isBannerVisible }"
+    />
+    <BangumiContent
+        @banner-visible="isBannerVisible = true"
+        @banner-invisible="isBannerVisible = false"
+    />
     <Bottom />
 </template>
 
 <style scoped>
 .bangumi-titlebar {
+    transition: background-color 0.3s ease;
+}
+.bangumi-titlebar.transparent {
     background-color: transparent;
 }
 </style>
