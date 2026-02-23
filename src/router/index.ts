01f4a706c4732466011a81cf5ad27090eb1a1d94
lolicer <13549177+lolicer@user.noreply.gitee.com>
2026-02-23 19:02:02 +0800

    更改网页title的位置，之后考虑使用unhead

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,80 +11,124 @@ const router = createRouter({
                 {
                     path: '',
                     name: 'home',
-                    component: Home
+                    component: Home,
+                    meta: {
+                        title: 'AcFun | 首页'
+                    }
                 },
                 {
                     path: 'donghua',
                     name: 'donghua',
                     component: () =>
-                        import('@/views/HomeView/CommonView/Donghua.vue')
+                        import('@/views/HomeView/CommonView/Donghua.vue'),
+                    meta: {
+                        title: 'AcFun | 首页-动画'
+                    }
                 },
                 {
                     path: 'lifestyle',
                     name: 'lifestyle',
                     component: () =>
-                        import('@/views/HomeView/CommonView/Lifestyle.vue')
+                        import('@/views/HomeView/CommonView/Lifestyle.vue'),
+                    meta: {
+                        title: 'AcFun | 首页-生活'
+                    }
                 },
                 {
                     path: 'music',
                     name: 'music',
                     component: () =>
-                        import('@/views/HomeView/CommonView/Music.vue')
+                        import('@/views/HomeView/CommonView/Music.vue'),
+                    meta: {
+                        title: 'AcFun | 首页-音乐'
+                    }
                 },
                 {
                     path: 'danceandidol',
                     name: 'danceandidol',
                     component: () =>
-                        import('@/views/HomeView/CommonView/DanceAndIdol.vue')
+                        import('@/views/HomeView/CommonView/DanceAndIdol.vue'),
+                    meta: {
+                        title: 'AcFun | 首页-舞蹈·偶像'
+                    }
                 },
                 {
                     path: 'gaming',
                     name: 'gaming',
                     component: () =>
-                        import('@/views/HomeView/CommonView/Gaming.vue')
+                        import('@/views/HomeView/CommonView/Gaming.vue'),
+                    meta: {
+                        title: 'AcFun | 首页-游戏'
+                    }
                 },
                 {
                     path: 'technology',
                     name: 'technology',
                     component: () =>
-                        import('@/views/HomeView/CommonView/Technology.vue')
+                        import('@/views/HomeView/CommonView/Technology.vue'),
+                    meta: {
+                        title: 'AcFun | 首页-科技'
+                    }
                 },
                 {
                     path: 'filmandtv',
                     name: 'filmandtv',
                     component: () =>
-                        import('@/views/HomeView/CommonView/FilmAndTV.vue')
+                        import('@/views/HomeView/CommonView/FilmAndTV.vue'),
+                    meta: {
+                        title: 'AcFun | 首页-影视'
+                    }
                 },
                 {
                     path: 'sports',
                     name: 'sports',
                     component: () =>
-                        import('@/views/HomeView/CommonView/Sports.vue')
+                        import('@/views/HomeView/CommonView/Sports.vue'),
+                    meta: {
+                        title: 'AcFun | 首页-体育'
+                    }
                 },
                 {
                     path: 'trending',
                     name: 'trending',
                     component: () =>
-                        import('@/views/HomeView/CommonView/Trending.vue')
+                        import('@/views/HomeView/CommonView/Trending.vue'),
+                    meta: {
+                        title: 'AcFun | 首页-鱼塘'
+                    }
                 },
                 {
                     path: 'article',
                     name: 'article',
-                    component: () => import('@/views/HomeView/Article.vue')
+                    component: () => import('@/views/HomeView/Article.vue'),
+                    meta: {
+                        title: 'AcFun | 首页-文章'
+                    }
                 }
             ]
         },
         {
             path: '/live',
             name: 'live',
-            component: () => import('@/views/LiveView/LiveLayout.vue')
+            component: () => import('@/views/LiveView/LiveLayout.vue'),
+            meta: {
+                title: 'AcFun | 直播'
+            }
         },
         {
             path: '/bangumi',
             name: 'bangumi',
-            component: () => import('@/views/BangumiView/BangumiLayout.vue')
+            component: () => import('@/views/BangumiView/BangumiLayout.vue'),
+            meta: {
+                title: 'AcFun | 番剧'
+            }
         }
     ]
+})
+
+router.afterEach((to) => {
+    const title = (to.meta.title as string) || 'AcFun'
+    document.title = title
 })
 
 export default router
--- src/views/LiveView/LiveLayout.vue
+++ src/views/LiveView/LiveLayout.vue
@@ -2,11 +2,6 @@
 import Bottom from '@/components/public/Bottom.vue'
 import TitleBar from '@/components/TitleBar/TitleBar.vue'
 import LiveContent from './LiveContent.vue'
-import { onBeforeMount } from 'vue'
-
-onBeforeMount(() => {
-    document.title = 'AcFun | 直播'
-})
 </script>
 
 <template>
